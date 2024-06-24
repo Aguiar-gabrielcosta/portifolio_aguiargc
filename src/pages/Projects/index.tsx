f644509b5700e8c198664247094bb318165c12b0
Gabriel Aguiar <Aguiar.gc@hotmail.com>
2024-06-24 13:31:53 -0300

    Adiciona o link de navegação para a página de contatos

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import ProjectCard from '../../components/ProjectCard'
 import Section from '../../components/Section'
 import * as S from './styles'
+import Button from '../../components/Button'
+import { useNavigate } from 'react-router-dom'
 
 type Project = {
   projectName: string
@@ -18,6 +20,7 @@ type Projects = {
 
 const Projects = () => {
   const [projectsList, setProjectsList] = useState<Projects>()
+  const navigate = useNavigate()
 
   useEffect(() => {
     fetch('./data/projects.json')
@@ -27,59 +30,70 @@ const Projects = () => {
 
   return (
     <Section title="Meus projetos">
-      <S.ProjectsContainer>
-        {projectsList?.projects.map((project) => (
+      <>
+        <S.ProjectsContainer>
+          {projectsList?.projects.map((project) => (
+            <ProjectCard
+              key={project.projectName}
+              projectName={project.projectName}
+              image={project.image}
+              tags={project.tags}
+              description={project.description}
+              deployLink={project.deployLink}
+              repoLink={project.repoLink}
+            />
+          ))}
           <ProjectCard
-            key={project.projectName}
-            projectName={project.projectName}
-            image={project.image}
-            tags={project.tags}
-            description={project.description}
-            deployLink={project.deployLink}
-            repoLink={project.repoLink}
+            image="https://placehold.co/300x300"
+            projectName="Nome do Projeto"
+            tags={['Tag 1', 'Tag 2']}
+            description="lorem lorem lorem sfasfasfsfas sfa sadfasdfasfsafsa sfd asf asfdfasf sadfasf safaf"
+            deployLink="#"
+            repoLink="#"
           />
-        ))}
-        <ProjectCard
-          image="https://placehold.co/300x300"
-          projectName="Nome do Projeto"
-          tags={['Tag 1', 'Tag 2']}
-          description="lorem lorem lorem sfasfasfsfas sfa sadfasdfasfsafsa sfd asf asfdfasf sadfasf safaf"
-          deployLink="#"
-          repoLink="#"
-        />
-        <ProjectCard
-          image="https://placehold.co/300x300"
-          projectName="Nome do Projeto"
-          tags={['Tag 1', 'Tag 2']}
-          description="lorem lorem lorem sfasfasfsfas sfa sadfasdfasfsafsa sfd asf asfdfasf sadfasf safaf sdgsagasgsagasg gasgasg gagdsg asdgasdg asdgsagagagasdgsagsag asdgsa asgdasg sg sg sg asg sgagagasgsd gsgsgasg asgasdgas gsagsag ag"
-          deployLink="#"
-          repoLink="#"
-        />
-        <ProjectCard
-          image="https://placehold.co/300x300"
-          projectName="Nome do Projeto"
-          tags={['Tag 1', 'Tag 2']}
-          description="lorem lorem lorem sfasfasfsfas sfa sadfasdfasfsafsa sfd asf asfdfasf sadfasf safaf"
-          deployLink="#"
-          repoLink="#"
-        />
-        <ProjectCard
-          image="https://placehold.co/300x300"
-          projectName="Nome do Projeto"
-          tags={['Tag 1', 'Tag 2']}
-          description="lorem lorem lorem sfasfasfsfas sfa sadfasdfasfsafsa sfd asf asfdfasf sadfasf safaf"
-          deployLink="#"
-          repoLink="#"
-        />
-        <ProjectCard
-          image="https://placehold.co/300x300"
-          projectName="Nome do Projeto"
-          tags={['Tag 1', 'Tag 2']}
-          description="lorem lorem lorem sfasfasfsfas sfa sadfasdfasfsafsa sfd asf asfdfasf sadfasf safaf sdgsagasgsagasg gasgasg gagdsg asdgasdg asdgsagagagasdgsagsag asdgsa asgdasg sg sg sg asg sgagagasgsd gsgsgasg asgasdgas gsagsag ag"
-          deployLink="#"
-          repoLink="#"
-        />
-      </S.ProjectsContainer>
+          <ProjectCard
+            image="https://placehold.co/300x300"
+            projectName="Nome do Projeto"
+            tags={['Tag 1', 'Tag 2']}
+            description="lorem lorem lorem sfasfasfsfas sfa sadfasdfasfsafsa sfd asf asfdfasf sadfasf safaf sdgsagasgsagasg gasgasg gagdsg asdgasdg asdgsagagagasdgsagsag asdgsa asgdasg sg sg sg asg sgagagasgsd gsgsgasg asgasdgas gsagsag ag"
+            deployLink="#"
+            repoLink="#"
+          />
+          <ProjectCard
+            image="https://placehold.co/300x300"
+            projectName="Nome do Projeto"
+            tags={['Tag 1', 'Tag 2']}
+            description="lorem lorem lorem sfasfasfsfas sfa sadfasdfasfsafsa sfd asf asfdfasf sadfasf safaf"
+            deployLink="#"
+            repoLink="#"
+          />
+          <ProjectCard
+            image="https://placehold.co/300x300"
+            projectName="Nome do Projeto"
+            tags={['Tag 1', 'Tag 2']}
+            description="lorem lorem lorem sfasfasfsfas sfa sadfasdfasfsafsa sfd asf asfdfasf sadfasf safaf"
+            deployLink="#"
+            repoLink="#"
+          />
+          <ProjectCard
+            image="https://placehold.co/300x300"
+            projectName="Nome do Projeto"
+            tags={['Tag 1', 'Tag 2']}
+            description="lorem lorem lorem sfasfasfsfas sfa sadfasdfasfsafsa sfd asf asfdfasf sadfasf safaf sdgsagasgsagasg gasgasg gagdsg asdgasdg asdgsagagagasdgsagsag asdgsa asgdasg sg sg sg asg sgagagasgsd gsgsgasg asgasdgas gsagsag ag"
+            deployLink="#"
+            repoLink="#"
+          />
+        </S.ProjectsContainer>
+        <S.ButtonContainer>
+          <Button
+            title="Clique para acessar a seção 'Projetos'"
+            type="button"
+            onClick={() => navigate('/contact')}
+          >
+            Entre em contato!
+          </Button>
+        </S.ButtonContainer>
+      </>
     </Section>
   )
 }
--- src/pages/Projects/styles.ts
+++ src/pages/Projects/styles.ts
@@ -5,3 +5,9 @@ export const ProjectsContainer = styled.div`
   grid-template-columns: 1fr 1fr 1fr;
   gap: 32px;
 `
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 32px;
+`
